c97056201635ca0eab6edc298b93dfc89f85e3d9
yesk993-ops <yesk993@gmail.com>
2026-03-10 19:35:29 +0530

    Add EnvController to return application color

--- src/main/java/com/example/bankapp/controller/EnvController.java
+++ src/main/java/com/example/bankapp/controller/EnvController.java
@@ -1,3 +1,9 @@
+package com.example.bankapp.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class EnvController {
 
